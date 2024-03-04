e7b08cc76b28a73a33c2a99f25a053218d97f4ca
muhammadalibscssef20 <muhammadali.bscssef20@iba-suk.edu.pk>
2024-03-04 12:04:31 +0500

    Task 4

--- lab 2/Task-4/app/src/main/java/com/example/task_4/MainActivity.kt
+++ lab 2/Task-4/app/src/main/java/com/example/task_4/MainActivity.kt
@@ -3,20 +3,80 @@ package com.example.task_4
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.task_4.ui.theme.Task4Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            
+            Container()
         }
     }
+}
+
+
+
+@Composable
+fun Container(){
+    val list = (1..4).map { it.toString() }
+    val image_List = listOf<Int>(R.drawable.img_1,R.drawable.img_2,R.drawable.img_3,R.drawable.img_4 )
+    val names = listOf<String>("Ali", "Khalil","Kashif","Zabi")
+
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(128.dp),
+
+        // content padding
+        contentPadding = PaddingValues(
+            start = 12.dp,
+            top = 16.dp,
+            end = 12.dp,
+            bottom = 16.dp
+        ),
+        content = {
+            items(list.size) { index ->
+                Card(
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .fillMaxWidth(),
+                ) {
+                    Column{
+                        Image(painter = painterResource(id = image_List[index]),
+                            contentDescription = "",
+                            modifier = Modifier.fillMaxWidth(1f).height(100.dp))
+                        Text(
+                            text = names[index],
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 30.sp,
+                            color = Color.Black,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.padding(4.dp)
+                        )
+                    }
+                }
+            }
+        }
+    )
 }
